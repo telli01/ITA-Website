166ac96855952b0a2c00ce17eaab7e63cc16cee9
telli <zirdeli1@hotmail.com>
2025-05-20 16:31:54 +0200

    cloudflare preset changed to "cloudflare_module"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-
+  nitro: {
+    preset: "cloudflare_module",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    }
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
